475b25b3648226b67ea5abccd6e1c6b1902e1126
Jesús Martínez <jesus.a.martinez.v@gmail.com>
2018-06-16 22:48:51 -0500

    Refactor.

--- src/main/scala/MNIST.scala
+++ src/main/scala/MNIST.scala
@@ -3,7 +3,7 @@ import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator
 import org.deeplearning4j.eval.Evaluation
 import org.deeplearning4j.nn.api.OptimizationAlgorithm
 import org.deeplearning4j.nn.conf.layers.DenseLayer
-import org.deeplearning4j.nn.conf.{NeuralNetConfiguration, Updater}
+import org.deeplearning4j.nn.conf.{MultiLayerConfiguration, NeuralNetConfiguration, Updater}
 import org.deeplearning4j.nn.conf.layers.OutputLayer
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork
 import org.deeplearning4j.nn.weights.WeightInit
@@ -18,7 +18,7 @@ object MNIST extends App {
 
   val numberOfOutputClasses = 10
 
-  // Hyper parameters
+  // Hyperparameters
   val batchSize = 128
   val randomSeed = 42
   val numberOfEpochs = 15
@@ -28,22 +28,7 @@ object MNIST extends App {
   val trainingSet = getTrainData(batchSize, randomSeed)
   val testSet = getTestData(batchSize, randomSeed)
 
-  // Network layers
-  val firstLayer = getHiddenLayer(numberOfColumns * numberOfRows, 1000)
-  val outputLayer = getOutputLayer(1000, numberOfOutputClasses)
-
-  // Network configuration
-  val networkConfiguration = new NeuralNetConfiguration.Builder()
-    .seed(randomSeed)
-    .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
-    .iterations(1)
-    .updater(Updater.ADAM)
-    .regularization(true).l2(regularizationRate)
-    .list(firstLayer, outputLayer)
-    .pretrain(false)
-    .backprop(true)
-    .build()
-
+  val networkConfiguration = getNerworkConfiguration
   // Actual model
   val network = new MultiLayerNetwork(networkConfiguration)
 
@@ -55,11 +40,27 @@ object MNIST extends App {
   evaluate(network, testSet, numberOfOutputClasses)
 
 
-
   private def getTrainData(batchSize: Int, seed: Int = 42) = new MnistDataSetIterator(batchSize, true, seed)
 
   private def getTestData(batchSize: Int, seed: Int = 42) = new MnistDataSetIterator(batchSize, false, seed)
 
+  private def getNerworkConfiguration: MultiLayerConfiguration = {
+    // Network layers
+    val firstLayer = getHiddenLayer(numberOfColumns * numberOfRows, 1000)
+    val outputLayer = getOutputLayer(1000, numberOfOutputClasses)
+
+    // Network configuration
+    new NeuralNetConfiguration.Builder()
+      .seed(randomSeed)
+      .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+      .iterations(1)
+      .updater(Updater.ADAM)
+      .regularization(true).l2(regularizationRate)
+      .list(firstLayer, outputLayer)
+      .pretrain(false)
+      .backprop(true)
+      .build()
+  }
   private def getHiddenLayer(inputSize: Int,
                              outputSize: Int,
                              activation: Activation = Activation.RELU,
